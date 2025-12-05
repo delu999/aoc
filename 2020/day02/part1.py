4d0eb645ac2fddfff2889e98bee89f7aeec55313
delu999 <delucariccardo@hotmail.com>
2025-12-06 00:06:51 +0100

    part 1 day 2 2020

--- 2020/day02/part1.py
+++ 2020/day02/part1.py
@@ -1,2 +1,17 @@
+solution = 0
 for line in open("./2020/day02/input.txt"):
-    print(line)
+    range, letter, password = line.split(" ")
+    start, end = range.split("-")
+    start, end = int(start), int(end)
+    letter = letter[0]
+    password.strip()
+    
+    count = 0
+    for e in password:
+        if e == letter:
+            count += 1
+    
+    if count >= start and count <= end:
+        solution += 1
+
+print(solution)
