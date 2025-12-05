7b669b3b49acf4fc88cdd8f07831660d0620a2ec
delu999 <delucariccardo@hotmail.com>
2025-12-05 02:30:39 +0100

    part 1 day1 2021

---
+++ 2021/day1/part1.py
@@ -0,0 +1,10 @@
+f = open("2021/day1/input.txt", "r")
+lines = f.readlines()
+
+answer = 0
+
+for i in range(1, len(lines)):
+    if int(lines[i]) > int(lines[i - 1]):
+        answer += 1
+
+print(answer)
