b23c2879ed017f795abb46f36d2f58bbe704d13e
delu999 <delucariccardo@hotmail.com>
2025-12-06 01:19:31 +0100

    part 2 day 4 2023

---
+++ 2023/day04/part2.py
@@ -0,0 +1,23 @@
+cards = []
+for i, line in enumerate(open("./2023/day04/input.txt")):
+    clean = line.split(": ")[1]
+    winning, numbers = clean.strip().split(" | ")
+    winning = [w for w in winning.split(" ") if len(w) != 0]
+    numbers = [w for w in numbers.split(" ") if len(w) != 0]
+
+    count = 0
+    for w in winning:
+        if w in numbers:
+            count += 1
+    cards.append(count)
+
+len_cards = len(cards)
+copies = [1 for _ in range(len_cards)]
+
+for i in range(0, len_cards):
+    for k in range(0, copies[i]):
+        for j in range(i + 1, i + 1 + cards[i]):
+            if j < len_cards:
+                copies[j] += 1
+sol = sum(copies)
+print(sol)
