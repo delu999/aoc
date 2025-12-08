8edfda308428c6baabd0a6bc8edd4f0b8c3fbd38
delu999 <delucariccardo@hotmail.com>
2025-12-08 19:34:40 +0100

    part 1 day08 2025

--- 2025/day08/part1.py
+++ 2025/day08/part1.py
@@ -1,85 +1,70 @@
 import math
 
+def main():
+    connections = 1000
+    lines = parse_input("./2025/day08/input.txt")
+    distances = calculate_distances(lines, connections)
+    solution = solve(distances)
+    print(solution)
+
+def parse_input(file_path):
+    lines: list[tuple[int, int, int]] = []
+    for l in open(file_path, "r"):
+        x, y, z = l.strip().split(",")
+        lines.append((int(x), int(y), int(z)))
+    return lines
+
+def calculate_distances(lines, connections):
+    distances: list[tuple[tuple[int, int, int], tuple[int, int, int], float]] = []
+    done = set()
+    for e1 in lines:
+        for e2 in lines:
+            if e1 == e2:
+                continue
+            if (e1, e2) not in done:
+                done.add((e1, e2))
+                done.add((e2, e1))
+                tmp = euclidean_distance(e1, e2)
+                distances.append((e1, e2, tmp))
+    distances.sort(key=lambda x: x[2])
+    return distances[:connections]
+
 def euclidean_distance(a: tuple[int, int, int], b: tuple[int, int, int]) -> float:
     px, py, pz = a
     qx, qy, qz = b
     return math.sqrt((px-qx)**2 + (py-qy)**2 + (pz-qz)**2)
 
-lines: list[tuple[int, int, int]] = []
-for l in open("./2025/day08/input.txt", "r"):
-    x, y, z = l.strip().split(",")
-    lines.append((int(x), int(y), int(z)))
-
-min_dist, node1, node2, groups = None, None, None, []
-
-distances: list[tuple[tuple[int, int, int], tuple[int, int, int], float]] = []
-for e1 in lines:
-    for e2 in lines:
-        if e1 == e2:
-            continue
-        tmp = euclidean_distance(e1, e2)
-        if not any(d[0] == e2 and d[1] == e1 for d in distances):
-            distances.append((e1, e2, tmp))
-
-distances.sort(key=lambda x: x[2])
-
-print(distances)
-
-groups = []
-counter = 0
-for a, b, _ in distances:
-    if counter == 1000:
-        break
-    found = False
-    for i, g in enumerate(groups):
-        if a in g and b in g:
-            found = True
-            break
-        elif a in g and b not in g:
-            for gg in groups:
-                if b in gg:
-                    g.update(gg)
-                    groups.remove(gg)
-                    break
-            g.add(b)
-            found = True
-            break
-        elif b in g and a not in g:
-            for gg in groups:
-                if a in gg:
-                    g.update(gg)
-                    groups.remove(gg)
-                    break
-            g.add(a)
-            found = True
-            break
-        elif a in g:
-            found = True
-            for gg in groups:
-                if b in gg:
-                    g.update(gg)
-                    groups.remove(gg)
-                    break
-            g.add(b)
-            break
-        elif b in g:
-            found = True
-            for gg in groups:
-                if a in gg:
-                    g.update(gg)
-                    groups.remove(gg)
-                    break
-            g.add(a)
-            break
-    if not found:
+def solve(distances) -> int:
+    groups = []
+    for a, b, _ in distances:
         groups.append(set([a, b]))
-    counter += 1
+        break
+    for a, b, _ in distances:
+        for g in groups:
+            if a in g and b in g:
+                break
+            elif a in g and b not in g:
+                for gg in groups:
+                    if b in gg:
+                        g.update(gg)
+                        groups.remove(gg)
+                        break
+                g.add(b)
+                break
+            elif b in g and a not in g:
+                for gg in groups:
+                    if a in gg:
+                        g.update(gg)
+                        groups.remove(gg)
+                        break
+                g.add(a)
+                break
+        else:
+            groups.append(set([a, b]))
 
-leng = []
+    leng = [len(g) for g in groups]
+    leng.sort(reverse=True)
+    return leng[0] * leng[1] * leng[2]
 
-for g in groups:
-    leng.append(len(g))
-
-leng.sort(reverse=True)
-print(leng)
-print(leng[0] * leng[1] * leng[2])
+if __name__ == '__main__':
+    main()
