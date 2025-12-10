28d572b75357efb73114673439b89cdbd4f5509d
delu999 <delucariccardo@hotmail.com>
2025-12-10 02:50:29 +0100

    part 2 day09 2025

--- 2025/day09/part2.py
+++ 2025/day09/part2.py
@@ -11,34 +11,18 @@ def parse_input(file_path: str) -> list[tuple[int, int]]:
     return points
     
 def solve(points: list[tuple[int, int]]) -> int:
-    area_points = gen_matrix(points)
-    print("done")
+    perimeter = gen_perimeter(points)
     points_w_area = points_with_area(points)
     
     for p1, p2, a in points_w_area:
-        x1, y1, x2, y2 = p1[0], p1[1], p2[0],  p2[1]
-
-        if x1 == x2:
-            if y1 > y2:
-                y2, y1 = y1, y2
-            if check_col(area_points, x1, y1, y2):
-                return a
-            continue
-        if y1 == y2:
-            if x2 > x1:
-                x2, x1 = x1, x2
-            if check_row(area_points, y1, x1, x2):
-                return a
-            continue
-
-        x1, y1, x2, y2 = swap_points(x1, y1, x2, y2)
-        if check_col(area_points, x1, y1, y2) and check_col(area_points, x2, y1, y2) and check_row(area_points, y1, x1, x2) and check_row(area_points, y2, x1, x2):
+        x1, y1, x2, y2 = swap_points(p1[0], p1[1], p2[0],  p2[1])
+        if check_col(perimeter, x1, y1, y2) and check_col(perimeter, x2, y1, y2) and check_row(perimeter, y1, x1, x2) and check_row(perimeter, y2, x1, x2):
             return a
     return -1
 
-def gen_matrix(points) -> set[tuple[int, int]]:
+def gen_perimeter(points) -> set[tuple[int, int]]:
     n = len(points)
-    tmp_perimeter = set()
+    perimeter = set()
     for i in range(n):
         x1, y1, x2, y2 = points[i][0], points[i][1], points[(i + 1) % n][0], points[(i + 1) % n][1]
         if x1 == x2:
@@ -46,45 +30,26 @@ def gen_matrix(points) -> set[tuple[int, int]]:
                 y1, y2 = y2, y1
             
             for k in range(y1, y2 + 1):
-                tmp_perimeter.add((k, x1))
+                perimeter.add((x1, k))
         else:
             if x1 > x2:
                 x1, x2 = x2, x1
             
             for k in range(x1, x2 + 1):
-                tmp_perimeter.add((y1, k))
-    
-    tmp_perimeter = list(tmp_perimeter)
-    nn = len(tmp_perimeter)
-    perimeter = set()
-    
-    for i in range(nn):
-        for j in range(i + 1, nn):
-            x, y1, x2, y2 = tmp_perimeter[i][0], tmp_perimeter[i][1], tmp_perimeter[j][0],  tmp_perimeter[j][1]
-            if x != x2:
-                continue
-            
-            if y1 > y2:
-                y2, y1 = y1, y2
-            
-            switch, ok = 0, False
-            for k in range(y1, y2 + 1):
-                if switch == 2:
-                    break
-                if (x, k) in tmp_perimeter:
-                    switch += 1
-                    ok = True
-                else:
-                    switch = 0
-                    ok = False
-            if ok:
-                for k in range(y1, y2 + 1):
-                    perimeter.add((x, k))
-    print("done3")
-    area = set(perimeter)
-    return area
+                perimeter.add((k, y1))
+    return perimeter
 
 def swap_points(x1, y1, x2, y2) -> tuple[int, int, int, int]:
+    if y1 == y2:
+        if x1 > x2:
+            return x2, y1, x1, y2
+        else:
+            return x1, y1, x2, y2
+    elif x1 == x2:
+        if y1 > y2:
+            return x1, y2, x2, y1
+        else:
+            return x1, y1, x2, y2
     if x1 > x2 and y1 > y2:
         return x2, y2, x1, y1
     if x1 > x2:
@@ -105,18 +70,37 @@ def points_with_area(points: list[tuple[int, int]]) -> list[tuple[tuple[int, int
     points_w_area.sort(key=lambda x: x[2], reverse=True)
     return points_w_area
 
-def check_row(area_points, x, y1, y2):
-    for i in range(y1, y2 + 1):
-        if (x, i) not in area_points:
+def check_col(perimeter, x, y1, y2):
+    in_out_in, ok = False, None
+    ## replace y1 since the starting point is always in the perimeter
+    for k in range(y1, y2 + 1):
+        if ok is not None and ok == False and in_out_in:
             return False
-    return True
+        if (x, k) in perimeter:
+            if ok == False:
+                in_out_in = True
+            ok = True
+        else:
+            ok = False
+    
+    if not ok:
+        return True
 
-def check_col(area_points, y, x1, x2):
-    for j in range(x1, x2 + 1):
-        if (j, y) not in area_points:
+
+def check_row(perimeter, y, x1, x2):
+    in_out_in, ok = False, None
+    for k in range(x1, x2 + 1):
+        if ok is not None and ok == False and in_out_in:
             return False
-    return True  
-
+        if (k, y) in perimeter:
+            if ok == False:
+                in_out_in = True
+            ok = True
+        else:
+            ok = False
+    
+    if not ok:
+        return True
 
 if __name__ == "__main__":
     main()
